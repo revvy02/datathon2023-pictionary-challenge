aef16c366a0deb041e4b4fd1055044b71c34f626
prithvi-patel-tamu <prith.vi@tamu.edu>
2023-10-28 15:55:52 -0500

    made processor class

--- src/main.py
+++ src/main.py
@@ -1,12 +1,19 @@
 from util.strokes_to_image import strokes_to_image
+from util.image_to_array import image_to_array
+from util.array_to_image import array_to_image
+
 import json
+import numpy as np
 
 with open("./pictionary_stroke_data/aircraft carrier.ndjson", "r") as file:
 	line = file.readline()
 	obj = json.loads(line)
 
 	img = strokes_to_image(obj["drawing"])
-	img.save("test.png", "PNG")
+	arr = image_to_array(img)
+
+	re_img = array_to_image(arr)
+	re_img.save("test.png", "PNG")
 	
 
 
